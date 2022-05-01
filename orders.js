f64d79f1f709dd88c4417d1b2b2b4fa1d50bfe62
Willard Ward <willardwardiii@gmail.com>
2022-05-01 11:58:18 -0500

    Finished orders

--- orders.js
+++ orders.js
@@ -31,6 +31,19 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
+    updateStatus (newStatus){
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`);
+    }
+}
+
 
 
 /*
@@ -46,6 +59,10 @@
 
 //CODE HERE
 
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], 1903, 575)
+
+console.log(firstTicket);
+
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +71,6 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking');
+console.log(firstTicket);
